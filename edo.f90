bacd619cd41cb5120fc3012f2cc09193844d3f96
TobiasDemoor <tobias.demoor@hotmail.com>
2019-10-29 15:32:39 -0300

    ndeah

--- edo.f90
+++ edo.f90
@@ -1,47 +1,56 @@
 module edo
 contains
-    function Vprima(V)
+    function Vprima(V, M)
         implicit none
-        real(8), dimension(0:4) :: Vprima, V
-        ! esto es relleno asi no se queja
-        real(8), parameter :: L1 = 10, L2 = 20, R = 1000, C = 0.001
+        real(8), allocatable :: Vprima(:,:,:)
+        real(8) :: V(:,:,:), M(:)
+        real(8), parameter :: G = 6.67384e-11
+        integer :: n, i, j
 
-        Vprima(0) = 1.0
-        Vprima(1) = V(2)
-        Vprima(2) = (sin(3*V(0)) + (V(3) - V(1)) / C) / L1
-        Vprima(3) = V(4)
-        Vprima(4) = ((V(1) - V(3)) / C - V(4) * R) / L2
-        ! ---------------------------------
+        n = size(V, dim = 2)
+        allocate(Vprima(0:3, 3, n))
+        Vprima(0,:,:) = 1.0
+        Vprima(1,:,:) = V(2,:,:) ! x' = v
+        Vprima(2,:,:) = V(3,:,:) ! v' = a
+        do i = 1, n
+            Vprima(3,:,i) = 0
+            do j = 1, i-1
+                Vprima(3,:,i) = Vprima(3,:,i) + G * M(i)*M(j)/sqrt((V(1,:,i)-V(1,:,j))**2)
+            end do
+            do j = i+1, n
+                Vprima(3,:,i) = Vprima(3,:,i) + G * M(i)*M(j)/sqrt((V(1,:,i)-V(1,:,j))**2)
+            end do
+        end do
     end function Vprima
     
-    function rk4SP(V, h)
-        real(8), dimension(:) :: V
-        real(8), allocatable, dimension(:)  :: rk4SP, K1, K2, K3, K4
+    function rk4SP(V, M, h)
+        real(8) :: V(:,:,:), M(:)
+        real(8), allocatable, dimension(:,:,:)  :: rk4SP, K1, K2, K3, K4
         real(8) :: h
-        integer :: orden
+        integer :: n
 
-        orden = size(V)
-        allocate(rk4SP(0:orden-1), K1(0:orden-1), K2(0:orden-1), K3(0:orden-1), K4(0:orden-1))
-        K1 = h*Vprima(V)
-        K2 = h*Vprima(V+K1/2.0)
-        K3 = h*Vprima(V+K2/2.0)
-        K4 = h*Vprima(V+K3)
+        n = size(V, dim = 2)
+        allocate(rk4SP(0:3, 3, n), K1(0:3, 3, n), K2(0:3, 3, n), K3(0:3, 3, n), K4(0:3, 3, n))
+        K1 = h*Vprima(V, M)
+        K2 = h*Vprima(V+K1/2.0, M)
+        K3 = h*Vprima(V+K2/2.0, M)
+        K4 = h*Vprima(V+K3, M)
         rk4SP = V + (K1 + 2.0*K2 + 2.0*K3 +K4)/6.0
         deallocate(K1, K2, K3, K4)
     end function
 
-    subroutine est1(V, h, tol)
+    subroutine est1(V, M, h, tol)
         implicit none
-        real(8), intent(in) :: V(:), tol
+        real(8), intent(in) :: V(:,:,:), M(:), tol
         real(8), intent(inout) :: h
         real(8) :: error
-        real(8), allocatable, dimension(:)  :: E
-        integer :: orden
+        real(8), allocatable :: E(:,:,:)
+        integer :: n
 
-        orden = size(V)
-        allocate(E(0:orden-1))
+        n = size(V, dim = 2)
+        allocate(E(0:3,3,n))
         do while(.true.)
-            E = rk4SP(V, h) - rk4SP(rk4SP(V, h/2), h/2)
+            E = rk4SP(V, M, h) - rk4SP(rk4SP(V, M, h/2), M, h/2)
             error = maxval(abs(E))
             if(error>=tol) then
                 h = h/2.0
@@ -56,27 +65,27 @@ contains
         90 print *
     end subroutine est1
 
-    subroutine rk4(Vi,  h, max, unit, format, hModif, tol)
+    subroutine rk4(Vi, M, h, max, unit, format, hModif, tol)
         implicit none
         logical,intent(in) :: hModif
         integer, intent(in) :: unit
-        real(8), intent(in) :: max, tol, Vi(:)
+        real(8), intent(in) :: max, tol, Vi(:,:,:), M(:)
         real(8), intent(inout) :: h
         character (len=8), intent(in):: format
-        real(8), allocatable, dimension(:)  :: V
-        integer :: orden
+        real(8), allocatable :: V(:,:,:)
+        integer :: n
 
-        orden = size(Vi)
-        allocate(V(0:orden-1))
+        n = size(Vi, dim = 2)
+        allocate(V(0:3,3,n))
         open(3, file = "h.txt")
         write(unit, format) Vi
         V = Vi
-        do while (V(0) <= max)
+        do while (V(0,1,1) <= max)
             if (hModif) then
                 write (3, '(F15.7)') h
-                call est1(V, h, tol)
+                call est1(V, M, h, tol)
             end if
-            V = rk4SP(V, h)
+            V = rk4SP(V, M, h)
             write (unit, format) V
         end do
         close(3)
