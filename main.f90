a377928d01fca73359cf172c3f05a487285cc4da
TobiasDemoor <tobias.demoor@hotmail.com>
2019-11-07 22:04:40 -0300

    Comentarios

--- main.f90
+++ main.f90
@@ -7,7 +7,7 @@ program main
     real(8) :: tfinal, h, tol = 1
 
     open(2, file = 'sistSolar.txt')
-    call LeeMasas(Vi, M, 2, 2)
+    call LeeMasas(Vi, M, 2, 2) ! en 2 dimensiones se ve mas lindo
     ! V(posicion/velocidad, x/y/z, masas)
     ! allocate(Vi(2,3,2),M(2))
     ! M = [dble(5e13), dble(1e11)]
@@ -20,7 +20,8 @@ program main
     ! ejecute con ajuste de h y vi que dentro de todo para el ejemplo que esta aca
     ! este es el mejor valor para todo t, igual con ajuste de h es mejor
     h = 2
-    tfinal = 3650*24*3600 ! el período orbital de la luna es de 24 dias
+    ! tfinal = 27*24*3600 ! el período orbital de la luna es de 27 dias
+    tfinal = 3650*24*3600 ! 10 años para ver orbitas externas
     call rk4(Vi, M, h, tfinal, .True., tol)
     call scriptGnuplot(size(Vi, dim = 3), size(Vi, dim = 2))
     call system("gnuplot -persist 'sgnptMasas.p'")
